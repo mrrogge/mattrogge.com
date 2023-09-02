46b6b81d43188a81c8346a85039b847c48ae3991
mrrogge <5900418+mrrogge@users.noreply.github.com>
2023-09-02 18:52:01 -0500

    add static header concept

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,20 @@
 <script lang="ts">
-    import ProfilePic from "$lib/img/matt-rogge-profile2.png";
-    import PlcPic from "$lib/img/plc.jpg";
-    import { fly } from "svelte/transition";
-
-    let skill_popup = false;
 </script>
+
+<div class="flex flex-row">
+	<div class="flex flex-col justify-between pt-2">
+		<div class="flex-1" />
+		<div class="border-t" />
+		<div class="flex-1 flex flex-col justify-end">
+			<div>and</div>
+		</div>
+	</div>
+	<div class="text-7xl align-text-top">I'M</div>
+	<div class="flex flex-col justify-between pt-2">
+		<div class="flex-1">Matt Rogge</div>
+		<div class="border-t" />
+        <div class="flex-1 flex flex-col justify-end">
+            <div>all about _____.</div>
+        </div>
+	</div>
+</div>
