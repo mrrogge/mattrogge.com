89a6aab5ef8904652bd66b6943f4ae9fc50e325b
mrrogge <5900418+mrrogge@users.noreply.github.com>
2023-09-03 12:58:26 -0500

    add experience info

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
 </script>
 
-<div class="flex-1">
+<div class="flex-1 flex flex-col">
 	<div
 		class="flex flex-row justify-center pt-24 pb-24 bg-[url('/src/lib/img/winter-city-view.png')] bg-cover bg-top"
 	>
@@ -21,4 +21,19 @@
 			</div>
 		</div>
 	</div>
+	<div class="p-16 flex flex-col gap-2">
+		<div class="flex flex-row text-slate-600 gap-2 align-middle">
+			<svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" class="bi bi-clock-fill" viewBox="0 0 16 16">
+				<path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM8 3.5a.5.5 0 0 0-1 0V9a.5.5 0 0 0 .252.434l3.5 2a.5.5 0 0 0 .496-.868L8 8.71V3.5z"/>
+			</svg>
+			<div class="text-2xl font-bold text-sky-700">12</div>
+			<div class="text-2xl">years experience</div>
+		</div>
+		<div class="flex flex-row text-slate-600 gap-2 ">
+			<svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" class="bi bi-geo-alt-fill" viewBox="0 0 16 16">
+				<path d="M8 16s6-5.686 6-10A6 6 0 0 0 2 6c0 4.314 6 10 6 10zm0-7a3 3 0 1 1 0-6 3 3 0 0 1 0 6z"/>
+			  </svg>
+			<div class="text-2xl">Minneapolis, MN</div>
+		</div>
+</div>
 </div>
