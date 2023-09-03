63810131607e8595a771b1a845151ea855f32874
mrrogge <5900418+mrrogge@users.noreply.github.com>
2023-09-02 22:29:16 -0500

    .

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,19 +3,19 @@
 
 <div class="flex-1">
 	<div class="flex flex-row justify-center pt-24 pb-24 bg-[url('/src/lib/img/winter-city-view.png')] bg-cover bg-top">
-		<div class="flex flex-col justify-between pt-2">
+		<!-- <div class="flex flex-col justify-between pt-2">
 			<div class="flex-1" />
 			<div class="border-t" />
 			<div class="flex-1 flex flex-col justify-end">
 				<div>and</div>
 			</div>
-		</div>
-		<div class="text-7xl align-text-top">I'M</div>
-		<div class="flex flex-col justify-between pt-2">
-			<div class="flex-1">Matt Rogge</div>
-			<div class="border-t" />
+		</div> -->
+		<div class="text-9xl align-text-top font-extralight text-slate-600">I'M</div>
+		<div class="flex flex-col justify-between pt-4">
+			<div class="flex-1 text-5xl text-cyan-700 font-bold">MATT ROGGE</div>
+			<div class="border-t-2 border-slate-600" />
 			<div class="flex-1 flex flex-col justify-end">
-				<div>all about _____.</div>
+				<div class="text-4xl pb-1 text-slate-600">all about <span class="underline" >programming</span>.</div>
 			</div>
 		</div>
 	</div>
