1d9250681573339b4349cc8e3559d571f1f550de
mrrogge <5900418+mrrogge@users.noreply.github.com>
2023-09-03 12:18:03 -0500

    remove "and"

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,13 +3,6 @@
 
 <div class="flex-1">
 	<div class="flex flex-row justify-center pt-24 pb-24 bg-[url('/src/lib/img/winter-city-view.png')] bg-cover bg-top">
-		<!-- <div class="flex flex-col justify-between pt-2">
-			<div class="flex-1" />
-			<div class="border-t" />
-			<div class="flex-1 flex flex-col justify-end">
-				<div>and</div>
-			</div>
-		</div> -->
 		<div class="text-9xl align-text-top font-extralight text-slate-600">I'M</div>
 		<div class="flex flex-col justify-between pt-4">
 			<div class="flex-1 text-5xl text-cyan-700 font-bold">MATT ROGGE</div>
