b76a428016df146622d3131723779543ec2b7cd5
mrrogge <5900418+mrrogge@users.noreply.github.com>
2023-09-03 21:14:37 -0500

    extract skill selector

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,5 @@
 <script lang="ts">
-	const skillMatrixWidth = 100;
-	const skillMatrixHeight = skillMatrixWidth * 0.8;
-	const skillMatrixX1 = (2 / 3) * skillMatrixWidth;
-	const skillMatrixY2 = (2 / 3) * skillMatrixHeight;
-	const skillMatrixX2 =
-		skillMatrixY2 / Math.tan(Math.PI / 2 - Math.atan(skillMatrixX1 / skillMatrixHeight));
+	import SkillSelector from "./SkillSelector.svelte";
 
 	let selectedSkill: 1 | 2 | 3 = 1;
 </script>
@@ -66,64 +61,7 @@
 	</div>
 	<div class="p-16 flex flex-col gap-2 border relative">
 		<div class="relative">
-			<svg
-				xmlns="http://www.w3.org/2000/svg"
-				width="40%"
-				height="auto"
-				fill="currentColor"
-				viewBox={`0 0 ${skillMatrixWidth} ${skillMatrixHeight}`}
-				on:click={() => console.log('clicked')}
-				on:keydown={() => console.log('keydown')}
-			>
-				<path
-					class=" fill-slate-600 [&.skillSelected]:fill-rose-400 hover:fill-rose-200"
-					class:skillSelected={selectedSkill === 1}
-					stroke="white"
-					stroke-opacity="1"
-					stroke-width="1"
-					d={`M 0 0 L ${skillMatrixX1} ${skillMatrixHeight} L 0 ${skillMatrixHeight} L 0 0`}
-					on:click={() => {
-						selectedSkill = 1;
-					}}
-					on:keydown={() => {
-						selectedSkill = 1;
-					}}
-				/>
-				<text x={skillMatrixX1/4} y={skillMatrixHeight *2/3 } font-size=4 text-anchor="middle" dominant-baseline="middle" pointer-events="none" >
-					Industrial
-				</text>
-				<text x={skillMatrixX1/4} y={skillMatrixHeight *2/3+ 4} font-size=4 text-anchor="middle" dominant-baseline="middle" pointer-events="none">automation</text>
-				<path
-					class="fill-slate-600 [&.skillSelected]:fill-teal-400 hover:fill-teal-200"
-					class:skillSelected={selectedSkill === 2}
-					stroke="white"
-					stroke-opacity="1"
-					stroke-width="1"
-					d={`M 0 0 L ${skillMatrixWidth} 0 L ${skillMatrixX2} ${skillMatrixY2} L 0 0 `}
-					on:click={() => {
-						selectedSkill = 2;
-					}}
-					on:keydown={() => {
-						selectedSkill = 2;
-					}}
-				/>
-				<text x={skillMatrixWidth/2} y={skillMatrixHeight/4} font-size=4 text-anchor="middle" dominant-baseline="middle" pointer-events="none">Programming</text>
-				<path
-					class="fill-slate-600 hover:fill-blue-200 [&.skillSelected]:fill-blue-400"
-					class:skillSelected={selectedSkill === 3}
-					stroke="white"
-					stroke-opacity="1"
-					stroke-width="1"
-					d={`M ${skillMatrixWidth} 0 L ${skillMatrixX2} ${skillMatrixY2} L ${skillMatrixX1} ${skillMatrixHeight} L ${skillMatrixWidth} ${skillMatrixHeight} L ${skillMatrixWidth} 0`}
-					on:click={() => {
-						selectedSkill = 3;
-					}}
-					on:keydown={() => {
-						selectedSkill = 3;
-					}}
-				/>
-				<text x={skillMatrixWidth * 3/4} y={skillMatrixHeight * 2/3} font-size=4 text-anchor="middle" dominant-baseline="middle" pointer-events="none">Design</text>
-			</svg>
+			<SkillSelector selectedSkill={selectedSkill} onSelect={index => selectedSkill = index} />
 		</div>
 	</div>
 </div>
