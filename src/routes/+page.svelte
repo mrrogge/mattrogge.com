1a2ea9a20c09369503141731c7e1aad710a69606
Matt Rogge <Matthew_R_Rogge@graco.com>
2023-09-05 10:15:07 -0500

    extract general info section

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
-	import Header from './Header.svelte';
+	import GeneralInfoSection from './GeneralInfoSection.svelte';
+import Header from './Header.svelte';
 import SkillSelector from './SkillSelector.svelte';
 
 	let selectedSkill: 1 | 2 | 3 = 1;
@@ -27,39 +28,7 @@ import SkillSelector from './SkillSelector.svelte';
 
 <div class="flex-1 flex flex-col">
 	<Header />
-	<div class="p-16 flex flex-col gap-2">
-		<div class="flex flex-row text-slate-600 gap-2 align-middle">
-			<svg
-				xmlns="http://www.w3.org/2000/svg"
-				width="32"
-				height="32"
-				fill="currentColor"
-				class="bi bi-clock-fill"
-				viewBox="0 0 16 16"
-			>
-				<path
-					d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM8 3.5a.5.5 0 0 0-1 0V9a.5.5 0 0 0 .252.434l3.5 2a.5.5 0 0 0 .496-.868L8 8.71V3.5z"
-				/>
-			</svg>
-			<div class="text-2xl font-bold text-sky-700">12</div>
-			<div class="text-2xl">years experience</div>
-		</div>
-		<div class="flex flex-row text-slate-600 gap-2">
-			<svg
-				xmlns="http://www.w3.org/2000/svg"
-				width="32"
-				height="32"
-				fill="currentColor"
-				class="bi bi-geo-alt-fill"
-				viewBox="0 0 16 16"
-			>
-				<path
-					d="M8 16s6-5.686 6-10A6 6 0 0 0 2 6c0 4.314 6 10 6 10zm0-7a3 3 0 1 1 0-6 3 3 0 0 1 0 6z"
-				/>
-			</svg>
-			<div class="text-2xl">Minneapolis, MN</div>
-		</div>
-	</div>
+	<GeneralInfoSection />
 	<div class="p-16 flex flex-col gap-2 border">
 		<div class="flex flex-row justify-between gap-12">
 			<div class="relative flex-1">
