289c27e1418643e5516f578c56b7a86c8ee75eed
Matt Rogge <Matthew_R_Rogge@graco.com>
2023-09-05 10:13:43 -0500

    extract header component

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
-	import SkillSelector from './SkillSelector.svelte';
+	import Header from './Header.svelte';
+import SkillSelector from './SkillSelector.svelte';
 
 	let selectedSkill: 1 | 2 | 3 = 1;
 
@@ -25,27 +26,7 @@
 </script>
 
 <div class="flex-1 flex flex-col">
-	<div
-		class="flex flex-row justify-center pt-24 pb-24 bg-[url('/src/lib/img/winter-city-view.png')] bg-cover bg-top"
-	>
-		<div
-			class="flex flex-row justify-center backdrop-brightness-110 backdrop-blur-sm drop-shadow-2xl rounded-2xl p-8"
-		>
-			<div class="text-9xl align-text-top font-extralight text-slate-600">I'M</div>
-			<div class="flex flex-col justify-between pt-4">
-				<div class="flex-1">
-					<span class="text-5xl text-sky-800 font-bold">MATT ROGGE</span>
-					<span class="text-5xl text-slate-600">.</span>
-				</div>
-				<div class="border-t-2 border-slate-600" />
-				<div class="flex-1 flex flex-col justify-end">
-					<div class="text-4xl pb-1 text-slate-600">
-						all about <span class="underline text-sky-800 font-bold">programming</span>.
-					</div>
-				</div>
-			</div>
-		</div>
-	</div>
+	<Header />
 	<div class="p-16 flex flex-col gap-2">
 		<div class="flex flex-row text-slate-600 gap-2 align-middle">
 			<svg
---
+++ src/routes/Header.svelte
@@ -0,0 +1,21 @@
+<div
+class="flex flex-row justify-center pt-24 pb-24 bg-[url('/src/lib/img/winter-city-view.png')] bg-cover bg-top"
+>
+<div
+    class="flex flex-row justify-center backdrop-brightness-110 backdrop-blur-sm drop-shadow-2xl rounded-2xl p-8"
+>
+    <div class="text-9xl align-text-top font-extralight text-slate-600">I'M</div>
+    <div class="flex flex-col justify-between pt-4">
+        <div class="flex-1">
+            <span class="text-5xl text-sky-800 font-bold">MATT ROGGE</span>
+            <span class="text-5xl text-slate-600">.</span>
+        </div>
+        <div class="border-t-2 border-slate-600" />
+        <div class="flex-1 flex flex-col justify-end">
+            <div class="text-4xl pb-1 text-slate-600">
+                all about <span class="underline text-sky-800 font-bold">programming</span>.
+            </div>
+        </div>
+    </div>
+</div>
+</div>
