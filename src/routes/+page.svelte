c3a0cff290dcb0d4912f398b93c43826ec60f84d
Matt Rogge <Matthew_R_Rogge@graco.com>
2023-09-06 17:22:36 -0500

    add more sections

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,6 +2,7 @@
 	import GeneralInfoSection from './GeneralInfoSection.svelte';
 import Header from './Header.svelte';
 	import SkillsSection from './SkillsSection.svelte';
+	import ProjectSection from './ProjectSection.svelte';
 
 </script>
 
@@ -9,4 +10,11 @@ import Header from './Header.svelte';
 	<Header />
 	<GeneralInfoSection />
 	<SkillsSection />
+	<ProjectSection />
+	<div class="p-16 mt-1 flex flex-col gap-2 border bg-zinc-100 rounded-md">
+		<p class="text-2xl w-1/4 text-center font-semibold">...but wait, there's more...</p>
+		<!-- 3 col full width, 1 col mobile -->
+
+	</div>
+	<div></div>
 </div>
