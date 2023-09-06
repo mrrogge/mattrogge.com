7f43fbd25cb6c9590935ff16a5c1d71aed6fa7f7
Matt Rogge <Matthew_R_Rogge@graco.com>
2023-09-06 17:21:52 -0500

    rework on skills section - move out projects, add design images

--- src/routes/SkillsSection.svelte
+++ src/routes/SkillsSection.svelte
@@ -2,8 +2,10 @@
 	import type { SkillEntry } from '../skills';
 	import SkillBadge from './SkillBadge.svelte';
 	import SkillSelector from './SkillSelector.svelte';
+	import GracoLogo from '$lib/img/graco_logo.svg';
+	import ProjectCard from './ProjectCard.svelte';
 
-	let selectedSkill: 1 | 2 | 3 = 1;
+	let selectedSkill: 1 | 2 | 3 = 3;
 
 	const industrialSkills: readonly SkillEntry[] = [
 		{ desc: 'PLC programming', level: 'EXPERT' },
@@ -35,88 +37,92 @@
 	] as const;
 </script>
 
-<div class="p-16 flex flex-col gap-2 border">
-	<div class="flex flex-row justify-between align-top gap-12">
+<div class="p-16 pt-6 mt-1 flex flex-col gap-6 border bg-zinc-100 rounded-md">
+	<p class="text-4xl font-semibold text-center w-1/3">SKILLS</p>
+	<div class="flex flex-row justify-between align-top gap-12 min-h-[575px]">
 		<div class="w-1/3">
 			<SkillSelector {selectedSkill} onSelect={(index) => (selectedSkill = index)} />
 		</div>
-		<div class="w-2/3 flex flex-col gap-4">
-			{#if selectedSkill === 1}
-				<div class="text-2xl">Industrial Automation: the catalyst</div>
-				<p>
-					While studying electrical engineering at the University of Minnesota, I started my
-					controls engineering career as a co-op student with PaR Systems. This led to a full-time
-					position where I honed my skills in system design and PLC programming.
-				</p>
-				<p>
-					I've had the pleasure of working on many fascinating industrial systems throughout my
-					career thus far. I strive to cast a wide net, learning about a variety of applications and
-					technologies. Aerospace, adhesive materials, food processing and packaging, fluid
-					handling...even pig farming!
-				</p>
-				<p>These are a few of the technologies I'm most familiar with:</p>
-				<div class="flex flex-row flex-wrap gap-4">
-					{#each industrialSkills as skill}
-						<SkillBadge {skill} />
-					{/each}
-				</div>
-				<p>Here are some of my most notable projects and achievements:</p>
-				<div class="flex flex-row gap-2">
-					<div class="w-1/4">GRACO IND SDKs</div>
-					<div class="w-3/4">
-						<ul class="list-disc list-inside">
-							<li>
-								Spearheaded the Software Development Kit (SDK) project for integrators. These
-								provided standardized code blocks and examples for controlling Graco products over a
-								fieldbus connection (e.g. from a PLC). 6 different products are supported across
-								both Rockwell and Siemens PLCs. Research showed these saved integrators 2-6 weeks of development time per project.
-							</li>
-							<li>
-								Created a custom build system for reproducing SDKs deterministically. This CLI tool
-								used Rockwell's ACM console software to generate .ACD files from an ACM project.
-							</li>
-							<li>
-								Programmed a parser for Siemen's SCL language that generates human-friendly
-								documentation (think doxygen for PLCs).
-							</li>
-							<li>Led various trainings and demostrations, including two hour-long sessions at the Annual Integrator Symposium.</li>
-						</ul>
+		<div class="w-2/3">
+			<div class="flex flex-col gap-4">
+				{#if selectedSkill === 1}
+				<div class="flex flex-col gap-4">
+					<div class="text-2xl font-semibold">Industrial Automation: the catalyst</div>
+					<p>
+						While studying electrical engineering at the University of Minnesota, I started my
+						controls engineering career as a co-op student with PaR Systems. This led to a full-time
+						position where I honed my skills in system design and PLC programming.
+					</p>
+					<p>
+						I've had the pleasure of working on many fascinating industrial systems throughout my
+						career thus far. I strive to cast a wide net, learning about a variety of applications
+						and technologies. Aerospace, adhesive materials, food processing and packaging, fluid
+						handling...even pig farming!
+					</p>
+					<p>These are a few of the technologies I'm most familiar with:</p>
+					<div class="flex flex-row flex-wrap gap-4">
+						{#each industrialSkills as skill}
+							<SkillBadge {skill} />
+						{/each}
 					</div>
 				</div>
-				<div class="flex flex-row gap-2">
-					<div class="w-1/4">GRACO CEO Award recipient 2021</div>
-					<div class="w-3/4">Given to those who demostrate high-quality work as recognized by their peers.</div>
+				{:else if selectedSkill === 2}
+				<div class="flex flex-col gap-4">
+					<div class="text-2xl font-semibold font-mono">Programming: the pillar</div>
+					<p>
+						Since my early childhood when I started writing BASIC on my mom's computer, I've been
+						obsessed with programming! There's nothing like solving a complex problem using
+						beautifully-written and well-organized code.
+					</p>
+					<p>
+						My peers have called me a "programming polyglot" due to the many different languages
+						I've explored:
+					</p>
+					<div class="flex flex-row flex-wrap gap-4">
+						{#each langs as langEntry}
+							<SkillBadge skill={langEntry} />
+						{/each}
+					</div>
 				</div>
-				<div class="flex flex-row gap-2">
-
+				{:else if selectedSkill === 3}
+				<div class="flex flex-col gap-4">
+					<div class="text-2xl font-semibold font-serif">Design: the glue</div>
+					<p>
+						Learned through necessity; maintained through passion. While not formally trained in
+						design, I've learned a lot on job designing HMIs, SCADA systems, and web/mobile
+						frontends. My style tends to be pragmatic and <span class="font-mono">functional</span>, but I still like to keep it <span
+							class="inline-block p-2 transform-gpu skew-x-12 -skew-y-3 italic font-mono font-semibold rounded-bl-xl rounded-tr-xl bg-gradient-to-r from-amber-300 to-sky-300 text-slate-950"
+							>fun</span
+						>.
+					</p>
+					<div class="flex flex-row justify-center bg-zinc-50 drop-shadow-sm border rounded-lg p-6 gap-12">
+						<div class="">
+							<img
+								src="src/lib/img/gtrack-graph.png"
+								class="h-[25rem]"
+								alt="screenshot of Graco Tracking mobile application"
+							/>
+						</div>
+						<div class="flex flex-col gap-4">
+							<div>
+								<img
+									src="src/lib/img/2k-hmi-controls.png"
+									class="h-[12rem]"
+									alt="screenshot of Graco 2K HMI application"
+								/>
+							</div>
+							<div>
+								<img
+									src="src/lib/img/the-glue-recursive.png"
+									class="h-[12rem]"
+									alt="recursive screenshot of this website"
+								/>
+							</div>
+						</div>
+					</div>
 				</div>
-			{:else if selectedSkill === 2}
-				<div class="text-2xl">Programming: the pillar</div>
-				<p>
-					Since my early childhood when I started writing BASIC on my mom's computer, I've been
-					obsessed with programming! There's nothing like solving a complex problem using
-					beautifully-written and well-organized code.
-				</p>
-				<p>
-					My peers have called me a "programming polyglot" due to the many different languages I've
-					explored:
-				</p>
-				<div class="flex flex-row flex-wrap gap-4">
-					{#each langs as langEntry}
-						<SkillBadge skill={langEntry} />
-					{/each}
-				</div>
-			{:else if selectedSkill === 3}
-				<div class="text-2xl">Design: the glue</div>
-				<p>
-					Learned through necessity; maintained through passion. While not formally trained in
-					design, I've learned a lot on job designing HMIs, SCADA systems, and web/mobile frontends.
-					My style tends to be pragmatic and functional, but I still like to keep it <span
-						class="inline-block p-2 transform-gpu skew-x-12 -skew-y-3 italic font-mono font-semibold rounded-bl-xl rounded-tr-xl bg-gradient-to-r from-amber-300 to-sky-300 text-slate-950"
-						>fun</span
-					>.
-				</p>
-			{/if}
+				{/if}
+			</div>
 		</div>
 	</div>
 </div>
