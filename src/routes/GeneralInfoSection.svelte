c2401eda882633878599b699ff6efe7f7e4db2b6
mrrogge <5900418+mrrogge@users.noreply.github.com>
2023-09-06 20:56:52 -0500

    add pic and education

--- src/routes/GeneralInfoSection.svelte
+++ src/routes/GeneralInfoSection.svelte
@@ -1,33 +1,61 @@
-<div class="p-16 flex flex-col gap-2 bg-zinc-100 drop-shadow-sm">
-    <div class="flex flex-row text-slate-600 gap-2 align-middle">
-        <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="32"
-            height="32"
-            fill="currentColor"
-            class="bi bi-clock-fill"
-            viewBox="0 0 16 16"
-        >
-            <path
-                d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM8 3.5a.5.5 0 0 0-1 0V9a.5.5 0 0 0 .252.434l3.5 2a.5.5 0 0 0 .496-.868L8 8.71V3.5z"
-            />
-        </svg>
-        <div class="text-2xl font-bold text-sky-700">12</div>
-        <div class="text-2xl">years experience</div>
-    </div>
-    <div class="flex flex-row text-slate-600 gap-2">
-        <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="32"
-            height="32"
-            fill="currentColor"
-            class="bi bi-geo-alt-fill"
-            viewBox="0 0 16 16"
-        >
-            <path
-                d="M8 16s6-5.686 6-10A6 6 0 0 0 2 6c0 4.314 6 10 6 10zm0-7a3 3 0 1 1 0-6 3 3 0 0 1 0 6z"
-            />
-        </svg>
-        <div class="text-2xl">Minneapolis, MN</div>
-    </div>
+<div class="p-16 bg-zinc-100 drop-shadow-sm">
+	<div class="flex flex-row gap-4 items-center">
+		<div>
+			<img src="src/lib/img/matt-rogge-profile.jpg" class="rounded-full w-[200px]" alt="Matt Rogge"/>
+		</div>
+		<div class="flex flex-col gap-2">
+			<div class="flex flex-row text-slate-600 gap-2 align-middle">
+				<svg
+					xmlns="http://www.w3.org/2000/svg"
+					width="32"
+					height="32"
+					fill="currentColor"
+					class="bi bi-clock-fill"
+					viewBox="0 0 16 16"
+				>
+					<path
+						d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM8 3.5a.5.5 0 0 0-1 0V9a.5.5 0 0 0 .252.434l3.5 2a.5.5 0 0 0 .496-.868L8 8.71V3.5z"
+					/>
+				</svg>
+				<div class="text-2xl font-bold text-sky-700">12</div>
+				<div class="text-2xl">years professional experience</div>
+			</div>
+			<div class="flex flex-row text-slate-600 gap-2">
+				<svg
+					xmlns="http://www.w3.org/2000/svg"
+					width="32"
+					height="32"
+					fill="currentColor"
+					class="bi bi-geo-alt-fill"
+					viewBox="0 0 16 16"
+				>
+					<path
+						d="M8 16s6-5.686 6-10A6 6 0 0 0 2 6c0 4.314 6 10 6 10zm0-7a3 3 0 1 1 0-6 3 3 0 0 1 0 6z"
+					/>
+				</svg>
+				<div class="text-2xl">Minneapolis, MN</div>
+			</div>
+			<div class="flex flex-row text-slate-600 gap-2 items-center">
+				<svg
+					xmlns="http://www.w3.org/2000/svg"
+					width="32"
+					height="32"
+					fill="currentColor"
+					class="bi bi-mortarboard-fill"
+					viewBox="0 0 16 16"
+				>
+					<path
+						d="M8.211 2.047a.5.5 0 0 0-.422 0l-7.5 3.5a.5.5 0 0 0 .025.917l7.5 3a.5.5 0 0 0 .372 0L14 7.14V13a1 1 0 0 0-1 1v2h3v-2a1 1 0 0 0-1-1V6.739l.686-.275a.5.5 0 0 0 .025-.917l-7.5-3.5Z"
+					/>
+					<path
+						d="M4.176 9.032a.5.5 0 0 0-.656.327l-.5 1.7a.5.5 0 0 0 .294.605l4.5 1.8a.5.5 0 0 0 .372 0l4.5-1.8a.5.5 0 0 0 .294-.605l-.5-1.7a.5.5 0 0 0-.656-.327L8 10.466 4.176 9.032Z"
+					/>
+				</svg>
+				<div class="flex flex-col">
+					<div class="text-2xl">Bachelor of Science Electrical Engineering</div>
+					<div class="text-2xl">University of Minnesota</div>
+				</div>
+			</div>
+		</div>
+	</div>
 </div>
