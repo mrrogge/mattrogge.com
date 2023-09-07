7784cad92edd636c225b5105753c5bd07fb84b4c
mrrogge <5900418+mrrogge@users.noreply.github.com>
2023-09-06 22:58:39 -0500

    create contact form

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,6 +3,7 @@
 import Header from './Header.svelte';
 	import SkillsSection from './SkillsSection.svelte';
 	import ProjectSection from './ProjectSection.svelte';
+	import ContactForm from './ContactForm.svelte';
 
 </script>
 
@@ -11,10 +12,5 @@ import Header from './Header.svelte';
 	<GeneralInfoSection />
 	<SkillsSection />
 	<ProjectSection />
-	<div class="p-16 mt-1 flex flex-col gap-2 border bg-zinc-100 rounded-md">
-		<p class="text-2xl w-1/4 text-center font-semibold">...but wait, there's more...</p>
-		<!-- 3 col full width, 1 col mobile -->
-
-	</div>
-	<div></div>
+	<ContactForm />
 </div>
---
+++ src/routes/ContactForm.svelte
@@ -0,0 +1,77 @@
+<script>
+	import { browser } from '$app/environment';
+</script>
+
+{#if browser}
+	<script src="https://web3forms.com/client/script.js" async defer></script>
+{/if}
+
+<div class="flex flex-col items-center">
+	<div class="max-w-md mx-auto my-10 bg-white p-5 rounded-md drop-shadow-sm">
+		<div class="text-center text-2xl font-bold m-2">Got questions? Let's get in touch.</div>
+		<div class="m-7">
+			<form action="https://api.web3forms.com/submit" method="POST" id="form">
+				<input type="hidden" name="access_key" value="b16e2f27-1031-41f6-a36d-5a12b11316cf" />
+				<input type="hidden" name="subject" value="New Submission from mattrogge.com" />
+				<input type="hidden" name="redirect" value="https://web3forms.com/success" />
+				<input type="checkbox" name="botcheck" id="" class="hidden" />
+				<div class="mb-6">
+					<label for="name" class="block mb-2 text-sm text-zinc-500">Full Name</label>
+					<input
+						type="text"
+						name="name"
+						id="name"
+						placeholder="Rusty Shackleford"
+						required
+						class="w-full px-3 py-2 placeholder-gray-300 border border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-sky-200 focus:border-sky-300"
+					/>
+				</div>
+				<div class="mb-6">
+					<label for="email" class="block mb-2 text-sm text-zinc-500">Email Address</label>
+					<input
+						type="email"
+						name="email"
+						id="email"
+						placeholder="you@company.com"
+						required
+						class="w-full px-3 py-2 placeholder-gray-300 border border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-sky-200 focus:border-sky-300"
+					/>
+				</div>
+				<div class="mb-6">
+					<label for="phone" class="text-sm text-zinc-500">Phone Number</label>
+					<input
+						type="text"
+						name="phone"
+						id="phone"
+						placeholder="+1 (555) 867-5309"
+						class="w-full px-3 py-2 placeholder-gray-300 border border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-sky-200 focus:border-sky-300"
+					/>
+				</div>
+				<div class="mb-6">
+					<label for="message" class="block mb-2 text-sm text-zinc-500">Your Message</label>
+
+					<textarea
+						rows="5"
+						name="message"
+						id="message"
+						placeholder="Your Message"
+						class="w-full px-3 py-2 placeholder-gray-300 border border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-sky-200 focus:border-sky-300"
+						required
+					/>
+				</div>
+				<div class="flex flex-row justify-center m-2">
+					<div class="h-captcha" data-captcha="true" />
+				</div>
+				<div class="mb-6">
+					<button
+						type="submit"
+						class="w-full px-3 py-4 text-white bg-sky-800 rounded-md focus:ring-sky-200 focus:border-sky-300"
+					>
+						Send Message
+					</button>
+				</div>
+				<p class="text-base text-center text-zinc-500" id="result" />
+			</form>
+		</div>
+	</div>
+</div>
