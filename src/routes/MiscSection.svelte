3890c8b8281fbfb7ba78b05e551cfca5db222493
mrrogge <5900418+mrrogge@users.noreply.github.com>
2023-09-06 22:57:59 -0500

    create misc section (future)

---
+++ src/routes/MiscSection.svelte
@@ -0,0 +1,4 @@
+<div class="p-16 mt-1 flex flex-col gap-2 border bg-zinc-100 rounded-md">
+    <p class="text-2xl w-1/4 text-center font-semibold">...but wait, there's more...</p>
+    <!-- 3 col full width, 1 col mobile -->
+</div>
