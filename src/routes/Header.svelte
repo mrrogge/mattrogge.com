214217daf7796eb1369c877efc48578f8db29319
mrrogge <5900418+mrrogge@users.noreply.github.com>
2023-09-12 20:57:35 -0500

    update header styles for mobile

--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -1,18 +1,22 @@
+<script lang="ts">
+
+</script>
+
 <div
-	class="flex flex-row justify-center pt-24 pb-24 bg-[url('/src/lib/img/winter-city-view.png')] bg-fixed bg-[length:120%_auto] bg-no-repeat bg-left-top"
+	class="flex flex-row justify-center pt-6 pb-6 lg:pt-12 lg:pb-12 bg-[url('/src/lib/img/winter-city-view.png')] bg-fixed bg-[length:1030px] lg:bg-[length:1500px] bg-no-repeat bg-top"
 >
 	<div
-		class="flex flex-row justify-center backdrop-brightness-110 backdrop-blur-sm drop-shadow-2xl rounded-2xl p-8"
+		class="flex flex-row justify-center backdrop-brightness-110 backdrop-blur-sm drop-shadow-2xl rounded-2xl p-4 lg:p-8"
 	>
-		<div class="text-9xl align-text-top font-extralight text-slate-600">I'M</div>
-		<div class="flex flex-col justify-between pt-4">
-			<div class="flex-1">
-				<span class="text-5xl text-sky-800 font-bold">MATT ROGGE</span>
-				<span class="text-5xl text-slate-600">.</span>
+		<div class="text-7xl lg:text-9xl align-text-top font-extralight text-slate-600">I'M</div>
+		<div class="flex flex-col justify-between pt-1 lg:pt-4">
+			<div class="">
+				<span class="text-2xl lg:text-5xl text-sky-800 font-bold">MATT ROGGE</span>
+				<span class="text-xl lg:text-5xl text-slate-600">.</span>
 			</div>
-			<div class="border-t-2 border-slate-600" />
-			<div class="flex-1 flex flex-col justify-end">
-				<div class="text-4xl pb-1 text-slate-600">
+			<div class="flex-1 border-t-2 translate-y-1/2 border-slate-600" />
+			<div class="flex flex-col justify-end">
+				<div class="text-lg lg:text-4xl lg:pb-1 text-slate-600">
 					all about <span class="underline text-sky-800 font-bold">programming</span>.
 				</div>
 			</div>
