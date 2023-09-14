f7d8d0ec9ef1b64be9d67175b9c52740408c4b61
mrrogge <5900418+mrrogge@users.noreply.github.com>
2023-09-13 21:51:37 -0500

    remove unnecessary text color classes

--- src/routes/GeneralInfoSection.svelte
+++ src/routes/GeneralInfoSection.svelte
@@ -15,31 +15,27 @@
 		</div>
 		<div class="flex flex-col gap-2">
 			<div class="flex flex-row gap-1">
-				<span class="w-[24px] h-[24px] lg:w-[32px] lg:h-[32px] text-slate-600">
+				<span class="w-[24px] h-[24px] lg:w-[32px] lg:h-[32px]">
 					<IconClockFill />
 				</span>
 				<div class="flex-1">
 					<span class="inline text-md lg:text-2xl font-bold text-sky-700">12</span>
-					<span class="inline text-md lg:text-2xl text-slate-600">
-						years professional experience
-					</span>
+					<span class="inline text-md lg:text-2xl"> years professional experience </span>
 				</div>
 			</div>
 			<div class="flex flex-row gap-1">
-				<span class="w-[24px] h-[24px] lg:w-[32px] lg:h-[32px] text-slate-600">
+				<span class="w-[24px] h-[24px] lg:w-[32px] lg:h-[32px]">
 					<IconGeoAltFill />
 				</span>
-				<span class="flex-1 text-md lg:text-2xl text-slate-600">Minneapolis, MN</span>
+				<span class="flex-1 text-md lg:text-2xl">Minneapolis, MN</span>
 			</div>
 			<div class="flex flex-row gap-1">
-				<span class="w-[24px] h-[24px] lg:w-[32px] lg:h-[32px] text-slate-600">
+				<span class="w-[24px] h-[24px] lg:w-[32px] lg:h-[32px]">
 					<IconMortarboardFill />
 				</span>
 				<div class="flex-1 flex flex-col">
-					<div class="text-md lg:text-2xl text-slate-600">
-						Bachelor of Science Electrical Engineering
-					</div>
-					<div class="text-md lg:text-2xl text-slate-600">University of Minnesota</div>
+					<div class="text-md lg:text-2xl">Bachelor of Science Electrical Engineering</div>
+					<div class="text-md lg:text-2xl">University of Minnesota</div>
 				</div>
 			</div>
 		</div>
--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -1,5 +1,4 @@
 <script lang="ts">
-
 </script>
 
 <div
@@ -8,15 +7,15 @@
 	<div
 		class="flex flex-row justify-center backdrop-brightness-110 backdrop-blur-sm drop-shadow-2xl rounded-2xl p-4 lg:p-8"
 	>
-		<div class="text-7xl lg:text-9xl align-text-top font-extralight text-slate-600">I'M</div>
+		<div class="text-7xl lg:text-9xl align-text-top font-extralight">I'M</div>
 		<div class="flex flex-col justify-between pt-1 lg:pt-4">
 			<div class="">
 				<span class="text-2xl lg:text-5xl text-sky-800 font-bold">MATT ROGGE</span>
-				<span class="text-xl lg:text-5xl text-slate-600">.</span>
+				<span class="text-xl lg:text-5xl">.</span>
 			</div>
 			<div class="flex-1 border-t-2 translate-y-1/2 border-slate-600" />
 			<div class="flex flex-col justify-end">
-				<div class="text-lg lg:text-4xl lg:pb-1 text-slate-600">
+				<div class="text-lg lg:text-4xl lg:pb-1">
 					all about <span class="underline text-sky-800 font-bold">programming</span>.
 				</div>
 			</div>
