1e03b85e410b21d45f7193c03cd77f184eee62fa
mrrogge <5900418+mrrogge@users.noreply.github.com>
2023-09-13 21:44:53 -0500

    comment out projects temporarily until wrapping issue is resolved

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,16 +1,15 @@
 <script lang="ts">
 	import GeneralInfoSection from './GeneralInfoSection.svelte';
-import Header from './Header.svelte';
+	import Header from './Header.svelte';
 	import SkillsSection from './SkillsSection.svelte';
 	import ProjectSection from './ProjectSection.svelte';
 	import ContactForm from './ContactForm.svelte';
-
 </script>
 
-<div class="flex-1 flex flex-col">
+<div>
 	<Header />
 	<GeneralInfoSection />
 	<SkillsSection />
-	<ProjectSection />
-	<ContactForm />
+	<!-- <ProjectSection /> -->
+	<!-- <ContactForm />  -->
 </div>
