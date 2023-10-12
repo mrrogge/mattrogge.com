5a9a3e4be119c70992282986e7798b30f7e95bc1
Matt Rogge <Matthew_R_Rogge@graco.com>
2023-10-11 20:25:51 -0500

    add section container component

---
+++ src/lib/SectionContainer.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	import { Styles } from '../styles';
+
+	let class_ = '';
+	export { class_ as class };
+</script>
+
+<div
+	class={`p-6 gap-4 rounded-xl text-slate-600 ${Styles.panelGradient} ${Styles.text1} ${class_}`}
+>
+	<slot />
+</div>
