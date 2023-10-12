2c133d6787997995652d99a54b76532afae30512
mrrogge <5900418+mrrogge@users.noreply.github.com>
2023-10-12 09:00:26 -0500

    use section container for experience

--- src/lib/ProjectSection.svelte
+++ src/lib/ProjectSection.svelte
@@ -1,10 +1,10 @@
 <script lang="ts">
-	import { Styles } from '../styles';
 	import ProjectCard from './ProjectCard.svelte';
 	import GracoLogoImg from '$lib/assets/graco_logo.svg';
+	import SectionContainer from './SectionContainer.svelte';
 </script>
 
-<div class={`p-16 mt-1 flex flex-col gap-2 ${Styles.panelGradient} rounded-md`}>
+<SectionContainer>
 	<p class="text-4xl w-1/4 text-center font-semibold">EXPERIENCE</p>
 	<ProjectCard>
 		<div slot="image" class="flex flex-col">
@@ -48,22 +48,21 @@
 			<div class="text-3xl font-extrabold text-center">BW Systems, Inc.</div>
 		</div>
 		<div>
-		<p>
-			Delivered a complex customized MES solution for a food-industry plant with a 24/7 production
-			schedule.
-		</p>
-		<ul class="list-disc list-inside">
-			<li>
-				Consisted of: 6 Rockwell PLCs, 3 grinding lines, 7 packaging lines, and 17 HMI thin clients. Handled SCADA and data storage through IA Ignition and SQL Server DB.
-			</li>
-			<li>
-				Contributed heavily to a code library of ~70k lines consisting of python and SQL.
-			</li>
-			<li>
-				Coordinated project with customer during their facility relocation and ERP migration.
-			</li>
-		</ul>
-	</div>
+			<p>
+				Delivered a complex customized MES solution for a food-industry plant with a 24/7 production
+				schedule.
+			</p>
+			<ul class="list-disc list-inside">
+				<li>
+					Consisted of: 6 Rockwell PLCs, 3 grinding lines, 7 packaging lines, and 17 HMI thin
+					clients. Handled SCADA and data storage through IA Ignition and SQL Server DB.
+				</li>
+				<li>Contributed heavily to a code library of ~70k lines consisting of python and SQL.</li>
+				<li>
+					Coordinated project with customer during their facility relocation and ERP migration.
+				</li>
+			</ul>
+		</div>
 	</ProjectCard>
 	<ProjectCard>
 		<div slot="image">
@@ -73,12 +72,14 @@
 		<div>
 			<ul class="list-disc list-inside">
 				<li>
-					Serving as Sergeant-at-Arms officer for 3 years running. This role involves facilitating weekly meetings for both in-person and remote members.
+					Serving as Sergeant-at-Arms officer for 3 years running. This role involves facilitating
+					weekly meetings for both in-person and remote members.
 				</li>
 				<li>
-					Working towards the Presentation Mastery Pathway (currently Level 2). Through this project, I've honed my public speaking ability by delivering 9 total speeches.
+					Working towards the Presentation Mastery Pathway (currently Level 2). Through this
+					project, I've honed my public speaking ability by delivering 9 total speeches.
 				</li>
 			</ul>
-	</div>
+		</div>
 	</ProjectCard>
-</div>
+</SectionContainer>
