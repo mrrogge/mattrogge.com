65393658891c02cd78af0617ed170a4800253005
mrrogge <5900418+mrrogge@users.noreply.github.com>
2023-10-13 21:10:16 -0500

    update section layout, add about me, limit max width

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,14 +4,18 @@
 	import SkillsSection from '../lib/skills/SkillsSection.svelte';
 	import ProjectSection from '../lib/ProjectSection.svelte';
 	import ContactForm from '../lib/ContactForm.svelte';
+	import AboutMeSection from '$lib/AboutMeSection.svelte';
 </script>
 
-<div class={`bg-[url("$lib/assets/pump-room.jpg")] bg-fixed bg-cover backdrop-blur-lg bg-no-repeat bg-top`}>
-	<Header />
-	<div class="pl-8 pr-8">
-		<GeneralInfoSection />
-		<SkillsSection />
-		<ProjectSection />
-		<ContactForm /> 
+<div class={`bg-[url("$lib/assets/pump-room.jpg")] bg-fixed bg-cover backdrop-blur-lg bg-no-repeat bg-top flex flex-col items-center`}>
+	<div class="max-w-screen-xl">
+		<Header />
+		<div class="pl-8 pr-8 flex flex-col gap-1">
+			<GeneralInfoSection />
+			<AboutMeSection />
+			<SkillsSection />
+			<ProjectSection />
+			<ContactForm /> 
+		</div>
 	</div>
 </div>
