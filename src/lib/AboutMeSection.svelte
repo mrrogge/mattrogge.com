453a022d309f62247fb1c7622a6f034de263ffb0
mrrogge <5900418+mrrogge@users.noreply.github.com>
2023-10-13 20:57:37 -0500

    add about me section

---
+++ src/lib/AboutMeSection.svelte
@@ -0,0 +1,19 @@
+<script>
+	import SectionContainer from "./SectionContainer.svelte";
+
+</script>
+
+<SectionContainer class="flex flex-col gap-2">
+    <h2 class="text-4xl text-center">ABOUT ME</h2>
+    <p>Throughout my life, I have consistently demonstrated a knack for problem-solving. From a young age, I expressed a natural curiosity for finding solutions to challenges. As a child, I started writing simple BASIC programs on my parents' home computer. This developed into a lifelong fascination with programming that continues to this day.</p>
+    <p>While studying electrical engineering at the University of Minnesota, I started my
+        controls engineering career as a co-op student with PaR Systems. This led to a
+        full-time position where I honed my skills in system design and PLC programming. </p>
+    <p>I've had the pleasure of working on many fascinating projects throughout my
+        career. I strive to cast a wide net, learning about a variety of applications
+        and technologies. Past projects have included aerospace, adhesive materials, food
+        processing and packaging, fluid handling...even pig farming!</p>
+    <p>I love learning new techniques, taking on challenges, and seeing disjointed parts become whole. There's really no better feeling. </p>
+    <p class="text-2xl font-bold text-center text-sky-800">Hard work. Integrity. Honesty.</p>
+    <p class="text-2xl text-center">These are my values.</p>
+</SectionContainer>
